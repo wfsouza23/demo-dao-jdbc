a0043b5535664ac74f8e3e8fa6d5e34b8f549d7f
Wesley Souza <wfsouza23@gmail.com>
2022-01-07 19:29:45 -0300

    update impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,10 +33,16 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("== TEST 3: seller findAll ====");
+        System.out.println("== TEST 4: seller insert ====");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Insert! New id = " + newSeller);
+
+        System.out.println("== TEST 5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update Completed!");
 
     }
 }
