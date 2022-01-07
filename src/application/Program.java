ba49c1884a9d46d0512220562729b83a198794e2
Wesley Souza <wfsouza23@gmail.com>
2022-01-07 19:20:50 -0300

    insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,5 +32,11 @@ public class Program {
         for(Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("== TEST 3: seller findAll ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Insert! New id = " + newSeller);
+
     }
 }
