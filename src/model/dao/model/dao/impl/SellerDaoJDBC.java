57a8d119780b228f54ad1e5b73f2d1405105bc5f
Wesley Souza <wfsouza23@gmail.com>
2022-01-07 19:39:56 -0300

    delete impl

--- src/model/dao/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/model/dao/impl/SellerDaoJDBC.java
@@ -81,8 +81,23 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public void deleteById(Seller obj) {
+    public void deleteById(Integer id) {
+        PreparedStatement st = null;
 
+        try{
+            st = conn.prepareStatement(
+                    "DELETE FROM seller\n" +
+                            "WHERE Id = ?");
+
+            st.setInt(1, id);
+
+            st.executeUpdate();
+
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
