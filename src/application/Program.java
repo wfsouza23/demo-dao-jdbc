f4ae61981c2190332b9efd1a563ec715f048e5eb
Wesley Souza <wfsouza23@gmail.com>
2022-01-07 18:57:02 -0300

    findByDeparment

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -15,7 +16,15 @@ public class Program {
 
         System.out.println("== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("== TEST 2: seller findByDepartment ====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
+        
     }
 }
